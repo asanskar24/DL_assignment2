8deafd4325f1eb7a98c6f8e51a57f3a62653ca21
asanskar24 <asanskar24@gmail.com>
2026-04-10 21:58:51 +0530

    k;lkjad

--- models/multitask.py
+++ models/multitask.py
@@ -50,7 +50,7 @@ class MultiTaskPerceptionModel(nn.Module):
         super().__init__()
 
         import gdown
-        gdown.download(id="<1L_wxb2k1i-EaKyfDbJMYzxDVf2X9-j3e>", output=classifier_path, quiet=False)
+        gdown.download(id="1L_wxb2k1i-EaKyfDbJMYzxDVf2X9-j3e", output=classifier_path, quiet=False)
         gdown.download(id="<localizer.pth drive id>", output=localizer_path, quiet=False)
         gdown.download(id="<unet.pth drive id>", output=unet_path, quiet=False)
 
